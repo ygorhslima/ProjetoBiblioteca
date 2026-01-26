bf897b0c4bb286c760da91e4c9b7183904b895ff
Ygor Henrique <ygorlima.dev06@gmail.com>
2026-01-26 12:12:53 -0300

    🐛 fix: corrigindo erro de overflow no BookList

--- ProjetoBiblioteca.FrontEnd/src/components/layout/SideBar/index.tsx
+++ ProjetoBiblioteca.FrontEnd/src/components/layout/SideBar/index.tsx
@@ -8,6 +8,7 @@ export default function SideBar({ isOpen }: SideBarProps) {
   return (
     <>
         <div className={`sidebar ${isOpen ? "open":"closed"}`}>
+          <h1>Biblioteca</h1>
           <div className="view-books">
             <Link to={`/livros`} className="link">
               Todos os livros
