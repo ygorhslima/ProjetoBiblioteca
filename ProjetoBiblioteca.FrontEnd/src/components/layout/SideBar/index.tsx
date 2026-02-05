aa91fb3032d09443b646ca78e922dc2025bd6ea8
Ygor Henrique <ygorlima.dev06@gmail.com>
2026-02-05 16:36:43 -0300

    ✨ feat: dropdown no sidebar para esconder e mostrar o catálogo

--- ProjetoBiblioteca.FrontEnd/src/components/layout/SideBar/index.tsx
+++ ProjetoBiblioteca.FrontEnd/src/components/layout/SideBar/index.tsx
@@ -1,42 +1,37 @@
 import { Link } from "react-router-dom";
 import "./style.css";
 import type SideBarProps from "../../../interfaces/SideBarProps";
-import { useEffect, useState } from "react";
-import type Area from "../../../interfaces/Area";
+import { useLibrary } from "../../../context/LibraryContext";
+import { ChevronDown } from "lucide-react";
+import { useState } from "react";
 
 export default function SideBar({ isOpen }: SideBarProps) {
-  const [areas, setAreas] = useState<Area[]>([]);
-  const buscarAreasLivros = async () => {
-    try {
-      const response = await fetch("http://localhost:5002/api/area");
-      if (!response.ok) throw new Error("Erro ao buscar dados");
-      const data = await response.json();
-      setAreas(data);
-    } catch (error) {
-      console.error("Erro ao buscar áreas:", error);
-    }
-  };
-
-  useEffect(() => {
-    buscarAreasLivros();
-  }, []);
-
+  const {areas} = useLibrary();
+  const [dropdown, setDropdown] = useState(false);
   return (
     <>
+
         <div className={`sidebar ${isOpen ? "open":"closed"}`}>
-          <div className="view-books">
-            <Link to={`/ProjetoBiblioteca/livros`} className="link">
-              Todos os livros
-            </Link>
+          
+          <div className="div_dropdown ">
+            <button onClick={()=>{setDropdown(!dropdown)}}><ChevronDown color="white"/></button>
+            <p style={{color:"white"}}>Catálogo</p>
           </div>
 
-          <div className="view-books">
-            {areas.map((el)=>(
-              <Link key={el.id} to={`/ProjetoBiblioteca/livros/${el.id}`} className="link">
-                {el.nome}
+         {dropdown ? <div>
+            <div className="view-books">
+              <Link to={`/ProjetoBiblioteca/livros`} className="link">
+                Todos os livros
               </Link>
-            ))}
-          </div>
+            </div>
+            <div className="view-books">
+              {areas.map((el)=>(
+                <Link key={el.id} to={`/ProjetoBiblioteca/livros/${el.id}`} className="link">
+                  {el.nome}
+                </Link>
+              ))}
+            </div>
+          </div>:<></>}
           
           <div className="view-books">
             <Link to={"/ProjetoBiblioteca/addlivros"} className="link">Adicionar livros</Link>
